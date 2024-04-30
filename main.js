908f41cb12ca218e4adad2caf8cdd00c5270d549
dimatk01 <dimatk0101@gmail.com>
2024-04-30 12:56:12 +0300

    init sequelize

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ const app = express();
 
 const swaggerPath =  path.resolve('docs', './docs.json');
 const swaggerDocument = fs.readFileSync(swaggerPath,'utf-8' );
-console.log(swaggerDocument)
 
 app.use('/docs', swaggerUi.serve,  swaggerUi.setup(JSON.parse(swaggerDocument), { swaggerOptions: {
         url: '/docs/docs.json',
@@ -27,13 +26,14 @@ const PORT = process.env.API_PORT ?? 3000;
  * Запуск додатку
  * @returns Екземпляр додатку
  */
-const start = () => {
+const start = async () => {
     try {
+        // await db.sequelize.sync();
         const server = app.listen(PORT, () => console.log(`App started at port: ${PORT}`));
 
         return server;
     } catch (e) {
-
+        console.log(e)
         process.exit(1);
     }
 }
